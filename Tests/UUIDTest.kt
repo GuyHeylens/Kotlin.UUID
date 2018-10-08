31c59852e091408bc7248ca6ed39410fbe35b048
guyheylens <guy@mosp.be>
2018-10-08 15:09:29 +0200

    Added JUnit Test

--- Tests/UUIDTest.kt
+++ Tests/UUIDTest.kt
@@ -1,3 +1,16 @@
 import org.junit.Assert.*
 
-class UUIDTest
+class UUIDTest{
+
+    @Test
+    fun simpleTest() {
+        Assert.assertEquals(2, 1 + 1)
+    }
+
+
+    @Test
+    fun test.empty.UUID(){
+        var uuid = UUID.empty()
+        Assert.assertEquals("00000000-0000-0000-0000-000000000000")
+    }
+}
